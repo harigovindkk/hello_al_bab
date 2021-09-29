b1f2a11ed9e9617a745e8480ff0911f00636bd8a
harigovindkk <harigovindkk008@gmail.com>
2021-09-29 13:51:37 +0530

    slider test

--- lib/screens/onboarding.dart
+++ lib/screens/onboarding.dart
@@ -2,100 +2,144 @@ import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class OnBoardingScreen extends StatefulWidget {
-  const OnBoardingScreen({Key? key}) : super(key: key);
+final List<String> imagesList = [
+  'https://cdn.pixabay.com/photo/2020/11/01/23/22/breakfast-5705180_1280.jpg',
+  'https://cdn.pixabay.com/photo/2016/11/18/19/00/breads-1836411_1280.jpg',
+  'https://cdn.pixabay.com/photo/2019/01/14/17/25/gelato-3932596_1280.jpg',
+  'https://cdn.pixabay.com/photo/2017/04/04/18/07/ice-cream-2202561_1280.jpg',
+];
 
+final List<String> titles = [
+  ' Coffee ',
+  ' Bread ',
+  ' Gelato ',
+  ' Ice Cream ',
+];
+
+class OnBoardingScreen extends StatefulWidget {
   @override
-  _OnBoardingScreenState createState() => _OnBoardingScreenState();
+  _MyAppState createState() => _MyAppState();
 }
 
-final List<String> imgList = [
-  'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
-  'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
-  'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
-  'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
-  'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
-  'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
-];
-
-final List<String> headingList = ['Search', 'Book', 'Go', '1', '2', '3'];
-final List<String> detailList = [
-  'Searchdetail',
-  'Bookdetail',
-  'Godetail',
-  '1detail',
-  '2detail',
-  '3detail'
-];
-
-class _OnBoardingScreenState extends State<OnBoardingScreen> {
-  final List<Widget> imageSliders = imgList
-      .map(
-        (item) => ClipRRect(
-          borderRadius: const BorderRadius.all(Radius.circular(8.0)),
-          child: Stack(
-            children: <Widget>[
-              Image.network(item, fit: BoxFit.cover, width: 1000.0),
-              Positioned(
-                bottom: 0.0,
-                left: 0.0,
-                right: 0.0,
-                child:  Container(
-                  decoration:const  BoxDecoration(
-                    gradient:  LinearGradient(
-                      colors: [
-                        Color.fromARGB(200, 0, 0, 0),
-                        Color.fromARGB(0, 0, 0, 0)
-                      ],
-                      begin: Alignment.bottomCenter,
-                      end: Alignment.topCenter,
-                    ),
-                  ),
-                  padding: const EdgeInsets.symmetric(
-                      vertical: 10.0, horizontal: 20.0),
-                  child: Column(
-                    children: [
-                      Text(
-                        headingList[imgList.indexOf(item)],
-                        style: GoogleFonts.poppins(color: Colors.black,
-                             fontSize: 20.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                       Text(
-                    detailList[imgList.indexOf(item)],
-                    style: GoogleFonts.poppins(color: Colors.black,
-                         fontSize: 15.0,
-
-                    ),
-                  ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      )
-      .toList();
+class _MyAppState extends State<OnBoardingScreen> {
+  int _currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Container(
-            color: Colors.white,
-            child: CarouselSlider(
+    return MaterialApp(
+      home: Scaffold(
+        body: Column(
+          children: [
+            CarouselSlider(
               options: CarouselOptions(
-                aspectRatio: 2.0,
-                enableInfiniteScroll: false,
-                initialPage: 1,
-                enlargeCenterPage: true,
-                scrollDirection: Axis.horizontal,
                 autoPlay: true,
+                // enlargeCenterPage: true,
+                //scrollDirection: Axis.vertical,
+                onPageChanged: (index, reason) {
+                  setState(
+                    () {
+                      _currentIndex = index;
+                    },
+                  );
+                },
               ),
-              items: imageSliders,
-            )),
+              items: imagesList
+                  .map(
+                    (item) => Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Card(
+                        margin: const EdgeInsets.only(
+                          top: 10.0,
+                          bottom: 10.0,
+                        ),
+                        elevation: 6.0,
+                        shadowColor: Colors.grey,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(100.0),
+                        ),
+                        child: ClipRRect(
+                          borderRadius: const BorderRadius.all(
+                            Radius.circular(100.0),
+                          ),
+                          child: Image.network(
+                            item,
+                            fit: BoxFit.cover,
+                            width: double.infinity,
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                  .toList(),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: imagesList.map((urlOfItem) {
+                int index = imagesList.indexOf(urlOfItem);
+                return Container(
+                  width: 10.0,
+                  height: 10.0,
+                  margin: const EdgeInsets.symmetric(
+                      vertical: 10.0, horizontal: 2.0),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: _currentIndex == index
+                        ? const Color.fromRGBO(0, 0, 0, 0.8)
+                        : const Color.fromRGBO(0, 0, 0, 0.3),
+                  ),
+                );
+              }).toList(),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.9,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50.0)),
+                  primary: Colors.blueAccent,
+                  padding: const EdgeInsets.all(15),
+                ),
+                onPressed: () {
+                  // Navigator.pushReplacement(
+                  //   context,
+                  //   MaterialPageRoute(
+                  //       builder: (context) => GoogleSignIn()),
+                  // );
+                },
+                child: Text(
+                  "Register",
+                  style: GoogleFonts.poppins(color: Colors.white, fontSize: 20),
+                ),
+              ),
+            ),
+           const SizedBox(
+              height: 20,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.9,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50.0)),
+                  primary: Colors.white,
+                  padding: const EdgeInsets.all(15),
+                ),
+                onPressed: () {
+                  // Navigator.pushReplacement(
+                  //   context,
+                  //   MaterialPageRoute(
+                  //       builder: (context) => GoogleSignIn()),
+                  // );
+                },
+                child: Text(
+                  "Login",
+                  style: GoogleFonts.poppins(
+                      color: Colors.blueAccent, fontSize: 20),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
