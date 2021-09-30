a92c0f010bb3da66cf33e8240cb61f3dc1a0934a
harigovindkk <harigovindkk008@gmail.com>
2021-09-30 12:58:55 +0530

    workspace detail demo

--- lib/screens/onboarding.dart
+++ lib/screens/onboarding.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -15,6 +16,7 @@ final List<String> titles = [
   ' Gelato ',
   ' Ice Cream ',
 ];
+final List<String> details =['detail1','detail2','detail3','detail4'];
 
 class OnBoardingScreen extends StatefulWidget {
   @override
@@ -26,121 +28,142 @@ class _MyAppState extends State<OnBoardingScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Column(
-          children: [
-            CarouselSlider(
-              options: CarouselOptions(
-                autoPlay: true,
-                // enlargeCenterPage: true,
-                //scrollDirection: Axis.vertical,
-                onPageChanged: (index, reason) {
-                  setState(
-                    () {
-                      _currentIndex = index;
-                    },
-                  );
-                },
-              ),
-              items: imagesList
-                  .map(
-                    (item) => Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Card(
-                        margin: const EdgeInsets.only(
-                          top: 10.0,
-                          bottom: 10.0,
+    return Scaffold(
+        body: Center(
+          child: Column(
+            children: [
+              SizedBox(height: MediaQuery.of(context).size.height*0.1,),
+              CarouselSlider(
+                options: CarouselOptions(height: 350,
+                  autoPlay: true,
+                   enlargeCenterPage: true,
+                  //scrollDirection: Axis.vertical,
+                  onPageChanged: (index, reason) {
+                    setState(
+                      () {
+                        _currentIndex = index;
+                      },
+                    );
+                  },
+                ),
+                items: imagesList
+                    .map(
+                      (item) => Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: 
+                        Column(
+                          children: [
+                            CircleAvatar(radius: 100.0,
+                              foregroundImage: NetworkImage(item),
+                            ),
+                            Text(titles[imagesList.indexOf(item)], style:GoogleFonts.poppins(fontSize: 20, fontWeight:FontWeight.bold)),
+                            const SizedBox(height: 10,),
+                            Text(details[imagesList.indexOf(item)],style:GoogleFonts.poppins(fontSize: 16,)),
+
+                          ],
                         ),
-                        elevation: 6.0,
-                        shadowColor: Colors.grey,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(100.0),
-                        ),
-                        child: ClipRRect(
-                          borderRadius: const BorderRadius.all(
-                            Radius.circular(100.0),
-                          ),
-                          child: Image.network(
-                            item,
-                            fit: BoxFit.cover,
-                            width: double.infinity,
-                          ),
-                        ),
+
+
+                        //  Card(
+                        //   margin: const EdgeInsets.only(
+                        //     top: 10.0,
+                        //     bottom: 10.0,
+                        //   ),
+                        //   elevation: 6.0,
+                        //   shadowColor: Colors.grey,
+                        //   shape: RoundedRectangleBorder(
+                        //     borderRadius: BorderRadius.circular(100.0),
+                        //   ),
+                        //   child: ClipRRect(
+                        //     borderRadius: const BorderRadius.all(
+                        //       Radius.circular(100.0),
+                        //     ),
+                        //     child:
+                        // Image.network(
+                        //       item,
+                        //       fit: BoxFit.cover,
+                        //       width: double.infinity,
+                        //     ),
+                        //   ),
+                        // ),
+
+                        // Card(
+                        //   margin: const EdgeInsets.only(
+                        //     top: 10.0,
+                        //     bottom: 10.0,
+                        //   ),
+                        //   elevation: 6.0,
+                        //   shadowColor: Colors.grey,
+                        //   shape: RoundedRectangleBorder(
+                        //     borderRadius: BorderRadius.circular(100.0),
+                        //   ),
+                        //   child: CircleAvatar(radius: 100.0,
+                        //       foregroundImage: NetworkImage(item),
+                        //     ),
+                        // ),
                       ),
+                    )
+                    .toList(),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: imagesList.map((urlOfItem) {
+                  int index = imagesList.indexOf(urlOfItem);
+                  return Container(
+                    width: 10.0,
+                    height: 10.0,
+                    margin: const EdgeInsets.symmetric(
+                        vertical: 10.0, horizontal: 2.0),
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: _currentIndex == index
+                          ? const Color.fromRGBO(0, 0, 0, 0.8)
+                          : const Color.fromRGBO(0, 0, 0, 0.3),
                     ),
-                  )
-                  .toList(),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: imagesList.map((urlOfItem) {
-                int index = imagesList.indexOf(urlOfItem);
-                return Container(
-                  width: 10.0,
-                  height: 10.0,
-                  margin: const EdgeInsets.symmetric(
-                      vertical: 10.0, horizontal: 2.0),
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: _currentIndex == index
-                        ? const Color.fromRGBO(0, 0, 0, 0.8)
-                        : const Color.fromRGBO(0, 0, 0, 0.3),
+                  );
+                }).toList(),
+              ),
+              Spacer(),
+              Container(
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  margin: EdgeInsets.all(10),
+                  child: ElevatedButton(
+                      style: ButtonStyle(
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18.0),
+                      ))),
+                      onPressed: () {},
+                      child: Text(
+                        "Register",
+                        style: GoogleFonts.poppins(),
+                      ))),
+                      
+              const SizedBox(
+                height: 5,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.8,
+                margin: EdgeInsets.all(10),
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.white),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18.0),
+                      ))),
+                  onPressed: () {},
+                  child: Text(
+                    "Login",
+                    style: GoogleFonts.poppins(color: Colors.black),
                   ),
-                );
-              }).toList(),
-            ),
-            Container(
-              width: MediaQuery.of(context).size.width * 0.9,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(50.0)),
-                  primary: Colors.blueAccent,
-                  padding: const EdgeInsets.all(15),
-                ),
-                onPressed: () {
-                  // Navigator.pushReplacement(
-                  //   context,
-                  //   MaterialPageRoute(
-                  //       builder: (context) => GoogleSignIn()),
-                  // );
-                },
-                child: Text(
-                  "Register",
-                  style: GoogleFonts.poppins(color: Colors.white, fontSize: 20),
                 ),
               ),
-            ),
-           const SizedBox(
-              height: 20,
-            ),
-            Container(
-              width: MediaQuery.of(context).size.width * 0.9,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(50.0)),
-                  primary: Colors.white,
-                  padding: const EdgeInsets.all(15),
-                ),
-                onPressed: () {
-                  // Navigator.pushReplacement(
-                  //   context,
-                  //   MaterialPageRoute(
-                  //       builder: (context) => GoogleSignIn()),
-                  // );
-                },
-                child: Text(
-                  "Login",
-                  style: GoogleFonts.poppins(
-                      color: Colors.blueAccent, fontSize: 20),
-                ),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
-      ),
-    );
+      );
+    
   }
 }
---
+++ lib/screens/workspace_detail.dart
@@ -0,0 +1,188 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:expandable_text/expandable_text.dart';
+
+class WorkSpaceDetail extends StatefulWidget {
+  const WorkSpaceDetail({Key? key}) : super(key: key);
+
+  @override
+  _WorkSpaceDetailState createState() => _WorkSpaceDetailState();
+}
+
+class _WorkSpaceDetailState extends State<WorkSpaceDetail> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Stack(
+        children: [
+          Container(
+            width: double.infinity,
+            height: 300,
+            decoration: const BoxDecoration(
+              color: Colors.blue,
+            ),
+            child: Image.network(
+              'https://picsum.photos/seed/118/600',
+              width: 100,
+              height: 100,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Align(
+            alignment: const AlignmentDirectional(20, 1.5),
+            child: Container(
+              width: double.infinity,
+              height: MediaQuery.of(context).size.height * 0.75,
+              decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30))),
+              child: Padding(
+                padding: const EdgeInsets.only(top :15.0, left: 20, right: 20 ),
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Text(
+                        'Workspace Name',
+                        style: GoogleFonts.poppins(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        'There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain...',
+                        style: GoogleFonts.poppins(color: Colors.black45),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      ExpandableText(
+                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+                        expandText: 'See More',
+                        collapseText: 'See Less',
+                        maxLines: 5,
+                        style: GoogleFonts.poppins(),
+                        linkColor: Colors.blue,
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        'Opening Timings',
+                        style: GoogleFonts.poppins(
+                            fontSize: 16, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const Text('Monday'),
+                              const Text('8.30 -17.30')
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                'Tuesday',
+                                style: GoogleFonts.poppins(),
+                              ),
+                              Text('8.30 -17.30', style: GoogleFonts.poppins())
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                'Wednesday',
+                                style: GoogleFonts.poppins(),
+                              ),
+                              Text('8.30 -17.30', style: GoogleFonts.poppins())
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                'Thursday',
+                                style: GoogleFonts.poppins(),
+                              ),
+                              Text('8.30 -17.30', style: GoogleFonts.poppins())
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                'Friday',
+                                style: GoogleFonts.poppins(),
+                              ),
+                              Text('8.30 -17.30', style: GoogleFonts.poppins())
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                'Saturday',
+                                style: GoogleFonts.poppins(),
+                              ),
+                              Text('8.30 -17.30', style: GoogleFonts.poppins())
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                'Sunday',
+                                style: GoogleFonts.poppins(),
+                              ),
+                              Text('8.30 -17.30', style: GoogleFonts.poppins())
+                            ],
+                          ),
+                        ],
+                      ),
+                     
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                              width: MediaQuery.of(context).size.width * 0.8,
+                              margin: EdgeInsets.all(10),
+                              child: ElevatedButton(
+                                  style: ButtonStyle(
+                                      shape: MaterialStateProperty.all<
+                                              RoundedRectangleBorder>(
+                                          RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(18.0),
+                                  ))),
+                                  onPressed: () {},
+                                  child: Text(
+                                    "Book Now",
+                                    style: GoogleFonts.poppins(),
+                                  ))),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
